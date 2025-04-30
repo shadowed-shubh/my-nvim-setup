4a406a349faac611b9a8da3abd270378b34d5851
shubh <shadowed.sp@gmail.com>
2025-04-30 12:31:20 +0530

    added new plugins and improved autocomplition and lsps

--- init.lua
+++ init.lua
@@ -20,6 +20,7 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.termguicolors = true
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format({async = false})]]
 -- Setup lazy.nvim
 require("lazy").setup("plugins",{
   spec = {
